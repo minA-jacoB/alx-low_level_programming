7bea682b26f0b60ccac78a079ca6d7b0088c3898
minA-jAcoB <mina.sameh.j@gmail.com>
2023-07-16 14:23:10 +0300

    mm

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle")
+int main(void){
+puts("\"Programming is like building a multilingual puzzle");
 
 
-	return(0)
+	return(0);
 }
 
