5a06491be94f96aaf1dcb46e67cb03ed565afb22
minA_jacoB <mina.sameh.j@gmail.com>
2023-07-18 01:07:20 +0300

    dfgh

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,17 +8,19 @@
 int main(void)
 {
 	int i, j;
-	
 	for (i = 48; i < 58; i++)
 	{
-		for (j = 48; j <58; j++)
+		for (j = 48; j < 58; j++)
 		{
 			if (j > i)
 			{
 				putchar(i);
 				putchar(j);
-				putchar(',');
-				putchar(' ');
+				if (i == 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
