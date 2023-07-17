14daa6cfec0508ef5917bf0384604e065aba0561
minA_jacoB <mina.sameh.j@gmail.com>
2023-07-17 17:57:59 +0300

    wert

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,12 +5,14 @@
  *
  * Return: Always 0 (Success)
  */
- int main(void)
+int main(void)
 {
 	int i;
+
 	for (i = 0; i < 10; i++)
 	{
-		printf("%d",i);
+		printf("%d" , i);
 	}
-	return(0);
+	printf("\n")
+	return (0);
 }
