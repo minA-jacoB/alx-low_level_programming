7961e4a88c0e3540976dc7dd50157d13c45b19f0
minA_jacoB <mina.sameh.j@gmail.com>
2023-07-18 01:29:45 +0300

    dfgh

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b ,c;
+
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 48; b < 58; b++);
+		{
+			for (c = 48; c < 58; c++);
+			{
+				if (c > b && b > a && a != b && a != c && b != c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 55)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+    return (0);
+}
