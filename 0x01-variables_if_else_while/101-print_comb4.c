d8bd4dfe0a9f171e2e8bcba5c6b29038795d1a89
minA_jacoB <mina.sameh.j@gmail.com>
2023-07-18 01:31:12 +0300

    dfgh

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,9 +11,9 @@ int main(void)
 
 	for (a = 48; a < 58; a++)
 	{
-		for (b = 48; b < 58; b++);
+		for (b = 48; b < 58; b++)
 		{
-			for (c = 48; c < 58; c++);
+			for (c = 48; c < 58; c++)
 			{
 				if (c > b && b > a && a != b && a != c && b != c)
 				{
