708d76f3a8c485002f6a221fd26af283704f1f45
minA_jacoB <mina.sameh.j@gmail.com>
2023-07-17 17:32:32 +0300

    second

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,12 @@
 
 int main(void)
 {
-        char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+        char alpha[26] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         int i;
 
         for (i = 0; i < 26; i++)
         {
                 putchar(alpha[i]);
-		putchar(toupper(alpha[i]));
         }
         putchar('\n');
         return (0);
