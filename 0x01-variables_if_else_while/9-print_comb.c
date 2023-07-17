2c7fa56b1afcef267417738e6de44429522cfff8
minA_jacoB <mina.sameh.j@gmail.com>
2023-07-17 19:04:46 +0300

    wsdfg

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 49; i < 59; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar(\n);
+	return (0);
+}
