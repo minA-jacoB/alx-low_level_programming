263744e3197717cee523e7e1e2b7559fea300604
minA_jacoB <mina.sameh.j@gmail.com>
2023-07-18 01:01:42 +0300

    dfgh

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+	
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j <58; j++)
+		{
+			if (j > i)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
