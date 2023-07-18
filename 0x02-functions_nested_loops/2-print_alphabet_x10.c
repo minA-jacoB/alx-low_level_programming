b505ab9dd21f3ac8e1d0ffd139bf387daa9c5e55
minA_jacoB <mina.sameh.j@gmail.com>
2023-07-19 00:10:46 +0300

    sdfg

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,8 +2,7 @@
 #include <stdio.h>
 /**
  * main - check the code.
- *
- * Return: Always 0.
+ * Return: Always 0
  */
 
 void print_alphabet_x10(void)
@@ -11,16 +10,16 @@ void print_alphabet_x10(void)
 	int i = 0;
 	char alpha;
 
-	while(i < 10)
+	while (i < 10)
 	{
 		alpha = 'a';
-		
-		while(alpha <= 'z')
+
+		while (alpha <= 'z')
 		{
 			putchar(alpha);
 			alpha++;
 		}
 		i++;
 		putchar('\n');
-	}	
+	}
 }
