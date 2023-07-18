56d727f82d13b8a169c69c7c2af35b9a554b1303
minA_jacoB <mina.sameh.j@gmail.com>
2023-07-18 17:13:28 +0300

    onee

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,12 +7,16 @@
  */
 int main(void)
 {
+    print_alphabet();
+    return (0);
+}
+void print_alphabet(void)
+{
     int i;
 
     for(i = 97; i < 123; i++)
     {
-	    putchar(i);
+        putchar(i);
     }
     putchar('\n');
-    return (0);
 }
