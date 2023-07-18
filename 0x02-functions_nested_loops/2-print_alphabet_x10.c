1eef27eba968cee4975e119a2e8ba6d799fe5052
minA_jacoB <mina.sameh.j@gmail.com>
2023-07-18 23:56:12 +0300

    sdfg

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+
+void print_alphabet_x10(void)
+{
+	int i = 0
+	char alpha = a;
+
+	while(i < 10)
+	{
+		alpha = a;
+		
+		while(ch <= z)
+		{
+			putchar(ch);
+			ch++;
+		}
+		i++;
+	}
+}
+
+		
+    return (0);
+}
