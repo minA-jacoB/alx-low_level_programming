584136378565e234ade085ca5706e51e499006d9
minA_jacoB <mina.sameh.j@gmail.com>
2023-07-25 02:00:41 +0300

    asdfg

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,18 @@
 #include "main.h"
-#include <string.h>
-
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
 int _strlen(char *s)
 {
-	int x = strlen(*s);
-	return (x);
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
 }
